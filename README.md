02cd209a959af7dcba8295659ab2f98bb3005fa2
Joseph Madsen <josephmads@protonmail.com>
2022-05-16 21:58:14 -0500

    Modified and new files added

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 This script moves files of a specific type into a new folder in the current directory.
 
 ## file-counter
-This script counts the type of each file and sorts the files into specific folders in the current directory if the number goes over a specified threshold.
+This script counts the type of each file and sorts the files into specific folders in the current directory if the number goes over a specified threshold. The file counts get written to a .txt and .csv file. <br>
+*This feature was to practice writing and reading files.*
 
 ## file-renamer
 This script renames files in a specified directory of a specified type.
--- file-counter.py
+++ file-counter.py
@@ -1,8 +1,8 @@
 # write a script that locates your Desktop
 
-from fileinput import filename
 import pathlib
 import pprint
+import csv
 
 desktop = pathlib.Path('/home/joe/Desktop')
 
@@ -13,10 +13,13 @@ jpg_folder = pathlib.Path('/home/joe/Desktop/jpgfolder')
 # and counts how many files of each different file type are on your desktop. 
 # Use a dictionary to collect this data, 
 
-file_dict = {'txt' : 0, 'jpg' : 0}
+file_dict = {'' : 0, 'txt' : 0, 'jpg' : 0}
 
 
 for filepath in desktop.iterdir():
+
+    if filepath.suffix == '':
+        file_dict[''] += 1
 
     if filepath.suffix == '.txt':
         file_dict['txt'] += 1
@@ -35,7 +38,19 @@ for filepath in desktop.iterdir():
             new_jpg_path = jpg_folder.joinpath(filepath.name)
             filepath.replace(new_jpg_path)
 
+with open('output.txt', 'a') as file_out:
+    file_out.write(str(file_dict))
+    file_out.write('\n')
     
-
 # and print it to your console at the end in order to get an overview of what is there.
 pprint.pprint(file_dict, indent=4)
+
+with open('output.txt', 'r') as file_in:
+    print(file_in.read())
+
+counts = {"": 8, ".csv": 2, ".md": 2, ".png": 11}
+
+with open("filecounts.csv", "a") as csvfile:
+    file_dict_writer = csv.writer(csvfile)
+    data = [file_dict[''], file_dict['txt'], file_dict['jpg']]
+    file_dict_writer.writerow(data) 
