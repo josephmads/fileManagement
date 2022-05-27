222394cb8a811d88e0c4cf246ea745d4bec177cb
Joseph Madsen <josephmads@protonmail.com>
2022-05-26 19:43:19 -0500

    Improved with list comprehension

--- file-renamer.py
+++ file-renamer.py
@@ -3,17 +3,14 @@
 
 import pathlib
 
-
 # set the directory containing the files to be renamed
 path = pathlib.Path("/home/joe/Pictures/examplePics/")
 
-# set the new name of the files
-file_name = "Pics"
-file_list = []
+file_name = "pics" # set the new name of the files
 counter = 1
 
-for file in sorted(path.glob("*.JPG")): # file type to rename
-    file_list.append(file)
+file_list = [file for file in sorted(path.glob("*.jpg"))] # file type to rename
+
 for file in file_list:
     new_file = file_name + str(counter) + str(file.suffix).lower()
     file.rename(path / new_file)
